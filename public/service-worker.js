c96724f4dbe56e28f4bb2e4392e95eb653cb1253
guipc <45241999+guipc@users.noreply.github.com>
2026-06-08 15:00:06 +0100

    Atualização de versão

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "festa-cache-v1.2";
+const CACHE_NAME = "festa-cache-v1.3";
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
@@ -6,6 +6,7 @@ const FILES_TO_CACHE = [
 ];
 
 self.addEventListener("install", event => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
     );
@@ -13,11 +14,14 @@ self.addEventListener("install", event => {
 
 self.addEventListener("activate", event => {
     event.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(
-                keys.map(key => key !== CACHE_NAME && caches.delete(key))
-            )
-        )
+        Promise.all([
+            caches.keys().then(keys =>
+                Promise.all(
+                    keys.map(key => key !== CACHE_NAME && caches.delete(key))
+                )
+            ),
+            clients.claim()
+        ])
     );
 });
 
